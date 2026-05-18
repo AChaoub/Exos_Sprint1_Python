1e5d36cefbaa33edbdb2f740a749b9a78822b3b1
AChaoub <achraf.chaoub@gmail.com>
2026-05-18 23:59:20 +0100

    exos

--- README.md
+++
@@ -1,1 +0,0 @@
-# Exos_Sprint1_Python
---
+++ example.py
@@ -0,0 +1,8 @@
+#  script
+# requires-python = ">=3.12"
+# dependencies = [
+#     "requests>=2.33.1",
+# ]
+
+import requests; 
+print(requests.get("https://astral.sh"))
